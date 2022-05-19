abab65fbb9b4818b34802852da2f84930824e535
miktdc <miktadcakmak@gmail.com>
2022-05-19 16:08:58 -0400

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -11,23 +11,27 @@ const Budget = () => {
     return ( 
         <div className = "Budget">
             <div className="flex-container">
-            
-                <PieChart width = { 550 }
-                height = { 550 } >
-                <Pie dataKey = "value"
-                isAnimationActive = { true }
-                data = { data }
-                cx = { 250 }
-                cy = { 200 }
-                outerRadius = { 200 }
-                fill = "#8884d8"
-                label />
-                <Tooltip/>
-                </PieChart>
-                <h1 > Budget </h1> 
-                <div className="flex-child"></div>
-                
+                <div className="flex-child">
+                    <PieChart width = { 550 }
+                    height = { 550 } >
+                    <Pie dataKey = "value"
+                    isAnimationActive = { true }
+                    data = { data }
+                    cx = { 250 }
+                    cy = { 200 }
+                    outerRadius = { 200 }
+                    fill = "#8884d8"
+                    label />
+                    <Tooltip/>
+                    </PieChart>
                 </div>
+                <h1 > Budget </h1>
+                <div className="flex-child">
+
+                </div>
+                
+                 
+            </div>
         </div>
     );
 };
