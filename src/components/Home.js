87d87346cd617a9164c30bbdbecf65790c1ee7ab
The-Vinh Calix Tran-Luu <xilacxilac@gmail.com>
2022-05-19 11:47:25 -0400

    men

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,18 @@ const Home = () => {
     <div className="main-container">
         <div className="flex-container">
             <div className="flex-child">
-                hi
+                <CategoryBox label="Savings">
+
+                </CategoryBox>
+                <CategoryBox label="Necessities">
+                    
+                </CategoryBox>
+                <CategoryBox label="Discretionary">
+                    
+                </CategoryBox>
+                <div className="showmore">
+                    <h2>Show More</h2>
+                </div>
             </div>
             <div className="flex-child">
                 hi1
@@ -14,6 +25,14 @@ const Home = () => {
         </div>
     </div>
   )
+}
+
+function CategoryBox(props) {
+    return (
+        <div className="categorybox">
+            <h2>{props.label}</h2>
+        </div>
+    );
 }
 
 export default Home
