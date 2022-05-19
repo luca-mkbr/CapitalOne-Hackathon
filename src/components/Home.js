7cc924e9f3f84ca1c8d514504541df613df5f19d
The-Vinh Calix Tran-Luu <xilacxilac@gmail.com>
2022-05-19 12:18:23 -0400

    men

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,36 +1,46 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { PieChart, Pie} from 'recharts';
 import './Home.css'
 
 const Home = () => {
-  return (
-    <div className="main-container">
-        <div className="flex-container">
-            <div className="flex-child">
-                <CategoryBox label="Savings">
+    const data = [
+        {name: 'Savings', percent:20 },
+        {name: 'Necessities', percent:50},
+        {name: 'Discretionary Spending', percent:30}
+    ];
+    return (
+        <div className="main-container">
+            <div className="flex-container">
+                <div className="flex-child">
+                    <CategoryBox label="Savings" amount="$69">
 
-                </CategoryBox>
-                <CategoryBox label="Necessities">
-                    
-                </CategoryBox>
-                <CategoryBox label="Discretionary">
-                    
-                </CategoryBox>
-                <div className="showmore">
-                    <h2>Show More</h2>
+                    </CategoryBox>
+                    <CategoryBox label="Necessities" amount="$69">
+                        
+                    </CategoryBox>
+                    <CategoryBox label="Discretionary" amount="$69">
+                        
+                    </CategoryBox>
+                    <div className="showmore">
+                    <Link to="/budget">Show More</Link>
+                    </div>
+                </div>
+                <div className="flex-child">
+                <PieChart width={700} height={700}>
+                    <Pie data={data} dataKey="percent" outerRadius={250} fill="yellow" />
+                </PieChart>
                 </div>
             </div>
-            <div className="flex-child">
-                hi1
-            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 function CategoryBox(props) {
     return (
         <div className="categorybox">
             <h2>{props.label}</h2>
+            {props.amount}
         </div>
     );
 }
