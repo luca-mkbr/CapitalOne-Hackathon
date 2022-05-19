8bf7e913960b997effd8641487ad1151191b7d61
Umran Koca <umrankoca4@gmail.com>
2022-05-19 11:29:15 -0400

    made the piechart, I think

---
+++ src/components/Budget.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { PieChart, Pie} from 'recharts';
+
+const Budget = () => {
+  const data = [
+      {name: 'Savings', percent:20 },
+      {name: 'Necessities', percent:50},
+      {name: 'Discretionary Spending', percent:30}
+  ];
+  return (
+      <PieChart width={700} height={700}>
+          <Pie data={data} dataKey="percent" outerRadius={250} fill="yellow" />
+      </PieChart>
+  );
+}
+
+export default Budget;
