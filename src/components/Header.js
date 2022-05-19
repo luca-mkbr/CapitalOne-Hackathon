e0475fe61dbb23a3b155c5fd151d71f080eb99d6
miktdc <miktadcakmak@gmail.com>
2022-05-19 17:27:38 -0400

    Update Header.js
    image

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,8 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header">
-            <Link to="/"><h1 className="appname">Capital One Budget Tracker</h1></Link>
+            <Link to="/"><h1 className="appname">CapitalOne Budget Tracker</h1></Link>
+            <img src = "https://d1.awsstatic.com/Customer%20References%20Directory%20Logos/Capital-One_Logo_%401x.0aef0b700254d3f618484ff03747100753de3d64.png"></img>
                 <div className="links">
                     <Link to="/home">Home</Link>
                     <Link to="/budget">Budget</Link>
