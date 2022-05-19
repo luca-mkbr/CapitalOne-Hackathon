40a5be1d9a4ebe9d17f4e5e6dcb707a0c098ce18
miktdc <miktadcakmak@gmail.com>
2022-05-19 12:15:19 -0400

    reward
    reward

--- src/components/Rewards.js
+++ src/components/Rewards.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import './Rewards.css'
 import Reward from './Reward.js'
+import {data} from '../data/data'
 
 const Rewards = () => {
-    const [data, setData] = useState([])
   return (
     <div>
         <h1 className='title'>Rewards</h1>
         <div className='rewards'>
-            {data.map((reward) => (
-                <Reward name={reward.name} image={reward.img} type={reward.type}/>
-            ))}
+          {data.map((reward) => (
+            <Reward name={reward.name} genre={movie.genre} image={movie.img}/>
+          ))}
         </div>
         <h2 className='subtitle'>Next Reward:</h2>
         <h1 className='title'>Past Rewards</h1>
