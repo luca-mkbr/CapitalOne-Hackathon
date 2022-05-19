5656e99971684a541ffe2a69d40f36cba16265cf
ryansribar <ryansribar@gmail.com>
2022-05-19 12:04:45 -0400

    Update Rewards.js

--- src/components/Rewards.js
+++ src/components/Rewards.js
@@ -3,11 +3,14 @@ import './Rewards.css'
 import Reward from './Reward.js'
 
 const Rewards = () => {
+    const [data, setData] = useState([])
   return (
     <div>
         <h1 className='title'>Rewards</h1>
         <div className='rewards'>
-            
+            {data.map((reward) => (
+                <Reward name={reward.name} image={reward.img} type={reward.type}/>
+            ))}
         </div>
         <h2 className='subtitle'>Next Reward:</h2>
         <h1 className='title'>Past Rewards</h1>
