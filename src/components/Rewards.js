d093ca84a1e5a6f3bcee5f2bfc6f1f24d24c60ea
ryansribar <ryansribar@gmail.com>
2022-05-19 12:18:13 -0400

    Update Rewards.js

--- src/components/Rewards.js
+++ src/components/Rewards.js
@@ -9,7 +9,7 @@ const Rewards = () => {
         <h1 className='title'>Rewards</h1>
         <div className='rewards'>
           {data.map((reward) => (
-            <Reward name={reward.name} iamge={reward.img} type={reward.type}/>
+            <Reward name={reward.name} image={reward.img} type={reward.type}/>
           ))}
         </div>
         <h2 className='subtitle'>Next Reward:</h2>
