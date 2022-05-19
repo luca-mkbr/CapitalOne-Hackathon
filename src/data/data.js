0faf48beb0529a18bb681ab5f0c0847aa467bec7
miktdc <miktadcakmak@gmail.com>
2022-05-19 12:05:20 -0400

    data
    data

---
+++ src/data/data.js
@@ -0,0 +1,15 @@
+export const data = [
+    {
+        img: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/1200px-Starbucks_Corporation_Logo_2011.svg.png",
+        name: "Starbucks"
+    },
+    {
+        img: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/de/WegmansLogo.svg/2560px-WegmansLogo.svg.png",
+        name: "Wegmans"
+    },
+    {
+        img: "https://media-exp1.licdn.com/dms/image/C560BAQF7nxKbzeFtLA/company-logo_200_200/0/1625776414346?e=2147483647&v=beta&t=bqFozy6HUaVMbOtg0FUwmeHufxflXByiuW3gx5jf60Q",
+        name: "The Watermark"
+    }
+
+]
