51002703917f0713e1464d478e2477335b2604d3
miktdc <miktadcakmak@gmail.com>
2022-05-19 12:15:32 -0400

    reward
    reward

--- src/components/Reward.js
+++ src/components/Reward.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import './Reward.css'
+import {data} from '../data/data.js'
 
-const Reward = () => {
+const Reward = ({name, image, type}) => {
   return (
     <div className="reward">
             <img src={image}></img>
             <p className="rewardName">{name}</p>
-            <p className="rewardActivate">{activate}</p>
+            <p className="rewardActivate">{type}</p>
     </div>
   )
 }
