f1149b9f7ed3019521693113de9487f2d8c9460b
ryansribar <ryansribar@gmail.com>
2022-05-19 11:22:34 -0400

    rewards

---
+++ src/components/Rewards.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Rewards.css'
+import Reward from './Reward.js'
+
+const Rewards = () => {
+  return (
+    <div>
+        <h1 className='title'>Rewards</h1>
+        <div className='rewards'>
+
+        </div>
+        <h1 className='title'>Past Rewards</h1>
+        <div className='rewards'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Rewards
