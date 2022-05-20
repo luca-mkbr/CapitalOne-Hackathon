0c163a6f5331535010b34e6dbaa351815d349047
Umran Koca <umrankoca4@gmail.com>
2022-05-20 00:43:48 -0400

    added an alert that says you got the right to rewards

--- src/components/Piechart.js
+++ src/components/Piechart.js
@@ -21,7 +21,7 @@ const Piechart = () => {
     { name: 'Loan Payments', value: loanPaymentsNew }
   ];
   console.log("piechart values:" + savingValue, necessitiesValue, discretionaryValue, loanPaymentsValue, total, savingNew, necessitiesNew, discretionaryNew, loanPaymentsNew, data)
-  if (savingNew>0.2 && necessitiesNew<0.5){ 
+  if (savingNew>0.2){ 
     alert("You have got the right for rewards!")
   }
   return (
